a168253b43ea37ef5593791bf5d20bdcbf8a98d4
Praval <jindalpraval791@gmail.com>
2023-03-23 15:59:23 +0530

    Bot css edit
    When add new message scroll to bottom

--- PW-hackathon-web/src/components/Bot.jsx
+++ PW-hackathon-web/src/components/Bot.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
 import { AiFillRobot } from "react-icons/ai";
 import { BiSend } from "react-icons/bi";
@@ -57,44 +57,45 @@ const Div = styled.div`
 const BotUI = () => {
   const [message, setMessage] = useState("");
   const [allMessages, setAllMessages] = useState([]);
+  const first = useRef(null);
 
   const handleClick = () => {
-    // e.preventDefault();
-    if (message) {
-      setAllMessages([
-        ...allMessages,
-        { text: message, ind: allMessages.length, type: "user" },
-      ]);
-    }
+    if (message)
+      setAllMessages([...allMessages, { msg: message, sendBy: "user" }]);
+
+    // send request to bot
     setMessage("");
-    console.log(allMessages);
   };
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
       handleClick();
     }
   };
+  useEffect(() => {
+    first.current.scrollTop = first.current?.scrollHeight;
+  }, [allMessages.length]);
+
   return (
     <div className="Bot">
-      <div className="top">
-        {/* {allMessages.map((msg, idx) => (
-          <div className="individualMessage" key={idx}>
-            {msg}
-          </div>
-        ))} */}
+      <div className="top" ref={first}>
         <div className="individualMessage bot">Hey</div>
         <div className="individualMessage bot">Hey how can i help you</div>
         <div className="individualMessage">Nothing! I'm just testing you</div>
         <div className="individualMessage bot">
           Ok! Just let me know if you have any query
         </div>
+        {allMessages.map(({ msg, sendBy }, idx) => (
+          <div className={`individualMessage ${sendBy}`} key={idx}>
+            {msg}
+          </div>
+        ))}
       </div>
       <div className="bottom">
         <input
           placeholder="How can I help You?"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
-          // onKeyPress={handleKeyPress}
+          onKeyPress={handleKeyPress}
         ></input>
         <button
           style={{ backgroundColor: "white", border: "none" }}
